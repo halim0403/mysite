e6054b242b475ee75ecb6e69b330de63ea971f4c
halim0403 <kim040339@gmail.com>
2025-03-31 16:05:58 +0900

    try

--- app.py
+++ app.py
@@ -1,11 +1,63 @@
 # 프레임워크 로드 
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, redirect
 import pandas as pd
 import invest
+from database import MyDB
+from dotenv import load_dotenv
+import os
+from data import querys
 
+# .env 파일 로드 
+load_dotenv()
 
 # Flask Class 생성 
 app = Flask(__name__)
+
+# database Class 생성 
+mydb = MyDB(
+    host = os.getenv('host'), 
+    port = int(os.getenv('port')), 
+    user = os.getenv('user'), 
+    pwd = os.getenv('pwd'), 
+    db = os.getenv('db')
+)
+
+# 테이블 생성하는 쿼리를 실행 
+mydb.execute_query(querys.create_query)
+
+# / api 생성
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# 회원 가입 페이지로 이동하는 api 생성 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+# 로그인 api
+
+# 회원 가입 api 
+@app.route('/signup2', methods=['post'])
+def signup2():
+    # id, password, name 데이터를 받아온다. 
+    input_id = request.form['id']
+    input_pass = request.form['password']
+    input_name = request.form['name']
+
+    # 사용가능한 아이디인가 확인 
+    check_result = mydb.execute_query(querys.check_query, input_id)
+    # check_result는 df
+    if len(check_result) == 0:
+        # 사용 가능한 아이디 
+        mydb.execute_query(querys.signup_query, 
+                            input_id, 
+                            input_pass, 
+                            input_name, 
+                            inplace = True)
+        return redirect("/")
+    else:
+        return redirect('signup')
 
 # 유저가 어떤 종목, 투자기간, 투자 전략 방식을 입력할수 있는 
 # 페이지를 보여주는 api 생성
